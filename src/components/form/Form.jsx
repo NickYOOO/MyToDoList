62c95dbe19f453d200c06a418be1589478883382
Nick YOOO <outgrowthr@gmail.com>
2023-06-13 17:26:48 +0900

    Design: Form component -> HTML,CSS 초안

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './form.css'
+
+function Form() {
+    return (
+        <form className='add-form'>
+            <div className='input-group'>
+                <label className='form-label'>제목</label>
+                <input
+                    className='add-input input-body'
+                    type="text"
+                ></input>
+                <label className='form-label'>내용</label>
+                <input
+                    className='add-input'
+                    type="text"
+                ></input>
+            </div>
+            <button className='add-button'>추가하기</button>
+        </form>
+    );
+};
+
+export default Form
--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from 'components/layout/Layout';
 import Header from 'components/header/Header';
+import Form from 'components/form/Form';
 
 function ToDoList() {
     return (
         <Layout>
             <Header />
+            <Form />
             <div>ToDoList</div>
         </Layout>
     );
