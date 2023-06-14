f529af55d3565b735c7b03c3b0188abfc699f4ca
Nick YOOO <outgrowthr@gmail.com>
2023-06-14 11:04:29 +0900

    Feat:[Form - component] -> eventHandler 기능 구현

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,6 +4,26 @@ import { useState } from 'react'
 import './form.css'
 
 function Form() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [id, setId] = useState(0);
+
+    const onChangeTitleHandler = (e) => {
+        setTitle(e.target.value);
+        console.log(e.target.value)
+    }
+
+    const onChangeBodyHandler = (e) => {
+        setBody(e.target.value);
+        console.log(e.target.value)
+    }
+
+    const onSunmitHandler = (e) => {
+        setId(id + 1);
+        setTitle('');
+        setBody('');
+    }
+
     return (
         <form className='add-form'>
             <div className='input-group'>
@@ -12,14 +32,18 @@ function Form() {
                     // class name  왜 ? 'add-input input-body'
                     className='add-input input-body'
                     type="text"
+                    value={title}
+                    onChange={onChangeTitleHandler}
                 ></input>
                 <label className='form-label'>내용</label>
                 <input
                     className='add-input'
                     type="text"
+                    value={body}
+                    onChange={onChangeBodyHandler}
                 ></input>
             </div>
-            <button className='add-button'>추가하기</button>
+            <button className='add-button' onClick={onSunmitHandler}>추가하기</button>
         </form>
     );
 };
