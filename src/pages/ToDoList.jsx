2f1ab91991444f955f2600772615e31920447a85
Nick YOOO <outgrowthr@gmail.com>
2023-06-15 01:36:45 +0900

    Feat : toDos 초기값( useState ) 입력 Feat: Form, List component에 상속

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from 'components/layout/Layout';
 import Header from 'components/header/Header';
 import Form from 'components/form/Form';
 import List from 'components/list/List';
 
-import ToDo from 'components/todo/ToDo';
-
 function ToDoList() {
+    const [toDos, setToDos] = useState([
+        {
+            id: 1,
+            title: "리액트 공부하기",
+            body: "리액트 기초를 공부해봅시다.",
+            isDone: false,
+        },
+        {
+            id: 2,
+            title: "리액트 공부하기",
+            body: "리액트 기초를 공부해봅시다.",
+            isDone: true,
+        }
+    ])
+
     return (
         <Layout>
             <Header />
-            <Form />
-            <List />
-            <ToDo />
+            <Form toDos={toDos} setToDos={setToDos} />
+            <List toDos={toDos} setToDos={setToDos} />
         </Layout>
     );
 };
