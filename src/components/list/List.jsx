637c3114c270e99f8e9c8cce104a9f3f74e5d5e3
Nick YOOO <outgrowthr@gmail.com>
2023-06-15 01:31:25 +0900

    Feat: deleteHandler / completeHandler/ props -> Todo.jsx에 상속등 기능구현 완료

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,16 +1,63 @@
 import React from 'react'
 import "./list.css"
+import ToDo from 'components/todo/ToDo';
 
-function List() {
+function List({ toDos, setToDos }) {
+
+    const deleteHandler = (selectedId) => {
+        const remainedTodos = toDos.filter((toDo) => {
+            return toDo.id !== selectedId
+        })
+        setToDos(remainedTodos)
+    }
+
+    const completeHandler = (selectedId) => {
+        const completeTodo = toDos.map((toDo) => {
+            if (toDo.id === selectedId) {
+                return { ...toDo, isDone: !toDo.isDone }
+            } else {
+                return { ...toDo }
+            }
+        })
+        setToDos(completeTodo)
+    }
+
     return (
         <div className='list-container'>
             <h2 className='list-title'>Working..🔥</h2>
-            <div className='list-wrapper'></div>
-
-            <h2 className='list-title'>Done..!🔥</h2>
-            <div className='list-wrapper'></div>
+            <div className='list-wrapper'>
+                {toDos.map((toDo) => {
+                    if (toDo.isDone === false) {
+                        return (
+                            <ToDo
+                                toDo={toDo}
+                                id={toDo.id}
+                                setToDos={setToDos}
+                                deleteHandler={deleteHandler}
+                                completeHandler={completeHandler}
+                            />
+                        );
+                    }
+                })}
+            </div>
+            <h2 className='list-title'>Done..!👍🏻</h2>
+            <div className='list-wrapper'>
+                {toDos.map((toDo) => {
+                    if (toDo.isDone === true) {
+                        return (
+                            <ToDo
+                                toDo={toDo}
+                                id={toDo.id}
+                                setToDos={setToDos}
+                                deleteHandler={deleteHandler}
+                                completeHandler={completeHandler}
+                            />
+                        );
+                    }
+                })}
+            </div>
         </div>
-    )
+    );
 }
 
 export default List
