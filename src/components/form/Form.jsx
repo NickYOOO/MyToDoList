e0aa7d0ee4d184efa9bd73db9b1c37f9935fe533
Nick YOOO <outgrowthr@gmail.com>
2023-06-23 17:06:59 +0900

    Fix:공백입력값일 때 중복되는 id값-> require을 통해 input에 빈 값을 방지

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -24,12 +24,14 @@ function Form({ toDos, setToDos }) {
             <div className='input-group'>
                 <label className='form-label'>제목</label>
                 <input
-                    // class name  왜 ? 'add-input input-body'
+                    // Trouble Shooting: 인풋창을 빈칸으로 두고 submit을 했을 시 id가 모두 0으로 출력되어 삭제/업데이트 시 한꺼번에 이벤트가 일어나는 오류 발생
+                    // 해결 방법: 유저가 인풋창에 값을 필수로 넣고 진행할 수 있게 required 속성 추가 
                     className='add-input input-body'
                     type="text"
                     name="title"
                     value={inputToDo.title}
                     onChange={onChangeHandler}
+                    required
                 ></input>
                 <label className='form-label'>내용</label>
                 <input
@@ -38,6 +40,7 @@ function Form({ toDos, setToDos }) {
                     name="body"
                     value={inputToDo.body}
                     onChange={onChangeHandler}
+                    required
                 ></input>
             </div>
             <button className='add-button'>추가하기</button>
